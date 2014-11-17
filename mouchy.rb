c7b2a9cb1805fd4dc2e27f3bd5bc26f89bff6183
Felix Delval <fdelval@ebay.com>
2014-11-17 12:45:57 +0100

    Adding mouchy to the repo

---
+++ mouchy.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class MagentoPhpcs < Formula
+    homepage 'https://github.com/fe-lix-/mouchy'
+    url 'https://github.com/fe-lix-/mouchy/archive/0.0.2.tar.gz'
+    sha1 '0fd41537538bc55216fcdcba9eee02b0fbd5f4ab'
+    depends_on 'php-code-sniffer'
+
+    def install
+        bin.install('bin/mouchy')
+    end
+
+    test do
+        system bin/'mouchy', '--version'
+    end
+end
