0ef2ecd4e87a7d43fe7eb2ccc3d2dde8cb72a8c6
Felix Delval <fdelval@ebay.com>
2014-11-17 12:48:40 +0100

    Updating SHA1 of mouchy

--- mouchy.rb
+++ mouchy.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Mouchy < Formula
     homepage 'https://github.com/fe-lix-/mouchy'
     url 'https://github.com/fe-lix-/mouchy/archive/0.0.2.tar.gz'
-    sha1 '0fd41537538bc55216fcdcba9eee02b0fbd5f4ab'
+    sha1 'e168f79b374306702d73ef8923f5af36e6a969f8'
     depends_on 'php-code-sniffer'
 
     def install
